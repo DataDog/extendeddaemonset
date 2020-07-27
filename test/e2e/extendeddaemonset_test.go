70ead9e294ff895373fc40c767779aae71b753f7
Celene <celene@datadoghq.com>
2020-07-27 14:35:49 -0400

    fix eds check

--- test/e2e/extendeddaemonset_test.go
+++ test/e2e/extendeddaemonset_test.go
@@ -191,11 +191,22 @@ func InitialDeployment(t *testing.T) {
 
 	t.Logf("CELENE checking EDS update")
 	edsTest := &datadoghqv1alpha1.ExtendedDaemonSet{}
-	if err := f.Client.Get(goctx.TODO(), types.NamespacedName{Name: daemonset.Name, Namespace: namespace}, edsTest); err != nil {
-		return false, nil
+	err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: daemonset.Name, Namespace: namespace}, edsTest)
+	if err != nil {
+		t.Logf("CELENE failed to get updated EDS")
 	}
-	t.Logf("CELENE output of EDS update, edsTest.Spec.Template.Spec.Containers:")
-	t.Logf(edsTest.Spec.Template.Spec.Containers)
+	resourceLimits := corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			// corev1.ResourceCPU:    resource.MustParse("0.001"),
+			corev1.ResourceMemory: resource.MustParse("1M"),
+		},
+	}
+	if diff := cmp.Diff(resourceLimits, edsTest.Spec.Template.Spec.Containers[0].Resources); diff != "" {
+		t.Logf("CELENE output of EDS diff:")
+		t.Logf(diff)
+	} else {
+		t.Logf("CELENE no diff between EDSs")
+	}
 
 	isPaused := func(dd *datadoghqv1alpha1.ExtendedDaemonSet) (bool, error) {
 		if val, ok := dd.Annotations[datadoghqv1alpha1.ExtendedDaemonSetCanaryPausedAnnotationKey]; ok {
