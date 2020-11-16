4755dcdb9a7eb7c60a69b8a5c2711f735b53d329
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2020-11-16 18:59:14 +0100

    Fix spec.strategy.ReconcileFrequency defaulting check (#65)
    the `spec.strategy.ReconcileFrequency` wasn't part of the `IsDefaultedExtendedDaemonSet()`
    function that check the `ExtendedDaemonSetSpec` was properly configured.
    
    This PR add a check in `IsDefaultedExtendedDaemonSet()` function to verify that
    the `spec.strategy.ReconcileFrequency` is properly defaulted.

--- api/v1alpha1/extendeddaemonset_default.go
+++ api/v1alpha1/extendeddaemonset_default.go
@@ -29,6 +29,10 @@ func IsDefaultedExtendedDaemonSet(dd *ExtendedDaemonSet) bool {
 		return false
 	}
 
+	if dd.Spec.Strategy.ReconcileFrequency == nil {
+		return false
+	}
+
 	if dd.Spec.Strategy.Canary != nil {
 		if defaulted := IsDefaultedExtendedDaemonSetSpecStrategyCanary(dd.Spec.Strategy.Canary); !defaulted {
 			return false
