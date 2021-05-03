7f8139372acc1abd2a2c5528724dc48a7c090976
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2021-05-03 16:38:14 +0200

    Merge the v0.6 release branch (#95)

--- main.go
+++ main.go
@@ -54,9 +54,11 @@ func init() {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
+	var leaderElectionResourceLock string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", true,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&leaderElectionResourceLock, "leader-election-resource", "configmaps", "determines which resource lock to use for leader election. option:[configmapsleases|endpointsleases|configmaps]")
 
 	// Custom flags
 	var printVersion, pprofActive bool
@@ -84,12 +86,13 @@ func main() {
 	version.PrintVersionLogs(setupLog)
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), config.ManagerOptionsWithNamespaces(setupLog, ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		HealthProbeBindAddress: ":8081",
-		Port:                   9443,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "extendeddaemonset-lock",
+		Scheme:                     scheme,
+		MetricsBindAddress:         metricsAddr,
+		HealthProbeBindAddress:     ":8081",
+		Port:                       9443,
+		LeaderElection:             enableLeaderElection,
+		LeaderElectionID:           "extendeddaemonset-lock",
+		LeaderElectionResourceLock: leaderElectionResourceLock,
 	}))
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
