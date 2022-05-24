0de75825b84dced1a89afd13b77bd3a69c0f3ab5
Julio Greff <julio.greff@datadoghq.com>
2022-05-24 16:18:31 +0200

    Fix CRD version
    Test CRD using v1 instead of v1beta1
    
    After the upgrade of controller-runtime, the deprecation of v1beta1 must
    have become effective, since integration tests no longer pass, erroring
    out with the following error message:
    
    ```
    unable to install CRDs onto control plane: unable to create CRD
    instances: unable to create CRD "extendeddaemonsets.datadoghq.com":
    CustomResourceDefinition.apiextensions.k8s.io
    "extendeddaemonsets.datadoghq.com" is invalid:
    spec.versions[0].schema.openAPIV3Schema: Required value: schemas are
    required
    ```
    
    Using v1 in the tests fixes the issue.

--- controllers/suite_e2e_test.go
+++ controllers/suite_e2e_test.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-2020 Datadog, Inc.
 
+//go:build e2e
 // +build e2e
 
 package controllers
@@ -15,7 +16,7 @@ import (
 func initTestConfig() *testConfigOptions {
 	return &testConfigOptions{
 		useExistingCluster: true,
-		crdVersion:         "v1beta1",
+		crdVersion:         "v1",
 		namespace:          fmt.Sprintf("eds-%d", time.Now().Unix()),
 	}
 }
