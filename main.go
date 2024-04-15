60abded0c839ddf79a4be87f67852773c739036a
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2024-04-15 12:16:35 +0200

    redirect klog to the zap logger

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/client-go/discovery"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	klog "k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	ctrlzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -46,6 +47,8 @@ var (
 )
 
 func init() {
+	klog.SetLogger(ctrl.Log.WithName("klog"))
+
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(datadoghqv1alpha1.AddToScheme(scheme))
@@ -220,7 +223,9 @@ func customSetupLogging(logLevel zapcore.Level, logEncoder string) error {
 	ctrl.SetLogger(ctrlzap.New(
 		ctrlzap.Encoder(encoder),
 		ctrlzap.Level(logLevel),
-		ctrlzap.StacktraceLevel(zapcore.PanicLevel)),
+		ctrlzap.StacktraceLevel(zapcore.PanicLevel),
+		ctrlzap.WriteTo(os.Stdout),
+	),
 	)
 
 	return nil
