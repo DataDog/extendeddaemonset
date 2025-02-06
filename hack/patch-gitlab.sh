4944615f00256aefe4d2fc3ba69063191d67b013
Celene <celene@datadoghq.com>
2025-02-06 11:13:54 -0500

    add lines for branch update

--- hack/patch-gitlab.sh
+++ hack/patch-gitlab.sh
@@ -6,9 +6,10 @@ SCRIPTS_DIR="$(dirname "$0")"
 source "$SCRIPTS_DIR/os-env.sh"
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
-FILE_PATH=$ROOT_DIR/.gitlab-ci.yml 
+GITLAB_FILE_PATH=$ROOT_DIR/.gitlab-ci.yml
+SERVICE_FILE_PATH=$ROOT_DIR/service.datadog.yaml
 
-# read arg as VERSION
+# Read arg as VERSION
 VERSION=$1
 
 if [ -z "$VERSION" ];
@@ -17,7 +18,12 @@ then
   exit 1
 fi
 
+# Update CONDUCTOR_BUILD_TAG envvar in Gitlab file, which is used in Conductor-triggered image build jobs
 # Replace . with -, add v
-VERSION=v$(echo $VERSION | sed "s/\./-/g")
+VERSION_SLUG=v$(echo $VERSION | sed "s/\./-/g")
 # Update envvar in Gitlab file
-$SED "s/CONDUCTOR_BUILD_TAG: .*$/CONDUCTOR_BUILD_TAG: $VERSION/g" $FILE_PATH
+$SED "s/CONDUCTOR_BUILD_TAG: .*$/CONDUCTOR_BUILD_TAG: $VERSION_SLUG/g" $GITLAB_FILE_PATH
+
+# Update branch that runs Conductor job, add v
+BRANCH=v$(echo $VERSION | sed -r "s/([^.]+.[^.]*).*/\1/")
+$SED "s/branch: .*$/branch: \"$BRANCH\"/g" $SERVICE_FILE_PATH
