4b9a3c797826ef333025d08ea7f8772f85e03dc7
Timothée Bavelier <97530782+tbavelier@users.noreply.github.com>
2025-08-27 15:21:38 +0200

    Include patch/update in EDS role for events (#217)
    * Include patch/update in EDS role for events
    
    * re-generate after re-installing tools

--- controllers/extendeddaemonset_controller.go
+++ controllers/extendeddaemonset_controller.go
@@ -32,7 +32,7 @@ type ExtendedDaemonSetReconciler struct {
 
 // +kubebuilder:rbac:groups=datadoghq.com,resources=extendeddaemonsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=datadoghq.com,resources=extendeddaemonsets/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups="",resources=events,verbs=create
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;update;patch
 // +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=services,verbs=get;watch
 // +kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch
