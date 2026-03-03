4bd2d560b9d791e75ae49811957497a4c2944940
Timothée Bavelier <timothee.bavelier@datadoghq.com>
2026-03-03 12:06:28 +0100

    Fix install-tools by replacing kubebuilder with controller-tools

---
+++ hack/install-controller-tools.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ROOT=$(git rev-parse --show-toplevel)
+WORK_DIR=$(mktemp -d)
+cleanup() {
+  rm -rf "$WORK_DIR"
+}
+trap "cleanup" EXIT SIGINT
+
+VERSION=$1
+
+TEST_DEPS_PATH=${2:-"$ROOT/bin/kubebuilder-tools"}
+
+if [ -z "$VERSION" ];
+then
+  echo "usage: bin/install-kubebuilder-tools.sh <version>"
+  exit 1
+fi
+
+
+OS=$(go env GOOS)
+ARCH=$(go env GOARCH)
+
+curl -L https://github.com/kubernetes-sigs/controller-tools/releases/download/envtest-v${VERSION}/envtest-v${VERSION}-${OS}-${ARCH}.tar.gz | tar -xz -C $WORK_DIR
+
+# move to repo_path/bin/kubebuilder-tools/bin - you'll need to set the KUBEBUILDER_ASSETS env var with
+rm -rf ${TEST_DEPS_PATH}
+mkdir -p ${TEST_DEPS_PATH}/bin
+mv $WORK_DIR/controller-tools/envtest/etcd ${TEST_DEPS_PATH}/bin/etcd
+mv $WORK_DIR/controller-tools/envtest/kube-apiserver ${TEST_DEPS_PATH}/bin/kube-apiserver
+mv $WORK_DIR/controller-tools/envtest/kubectl ${TEST_DEPS_PATH}/bin/kubectl
